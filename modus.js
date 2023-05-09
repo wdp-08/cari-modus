935b8262c3c5d285f6dcf53a3b016b8d2ca37025
SalmO-On <fitrianurul0213@gmail.com>
2023-05-09 21:15:09 +0700

    hasil

---
+++ modus.js
@@ -0,0 +1,22 @@
+function cariModus(arr) {
+    let arrModus = [];
+
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                arrModus.push(arr[i]);
+            } else {
+                false;
+            }
+        }
+    }
+    return (arrModus[0] === undefined || false) ? -1 : arrModus[0];
+}
+
+
+console.log(cariModus([10, 4, 5, 2, 4])); // 4
+console.log(cariModus([5, 10, 10, 6, 5])); // 5
+console.log(cariModus([10, 3, 1, 2, 5])); // -1
+console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+console.log(cariModus([7, 7, 7, 7, 7])); // -1
