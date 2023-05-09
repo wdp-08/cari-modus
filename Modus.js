6db69588c202bd570a563a990bd4601a25540a58
gemagheina19 <gheina.gemalia@ui.ac.id>
2023-05-09 23:49:04 +0700

    Coba Cari Modus

---
+++ Modus.js
@@ -0,0 +1,27 @@
+function cariModus(arr) {
+    let saveNum = {};
+      let maxCount = 0;
+      let modus = [];
+    
+      for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+        saveNum[num] = (saveNum[num] || 0) + 1;
+        if (saveNum[num] > maxCount) {
+          maxCount = saveNum[num];
+        }
+      }
+    
+      for (let num in saveNum) {
+        if (saveNum[num] === maxCount) {
+          modus.push(num);
+        }
+      }
+    
+      return modus;
+    }
+    
+  console.log(cariModus([10, 4, 5, 2, 4])); 
+  console.log(cariModus([5, 10, 10, 6, 5])); 
+  console.log(cariModus([10, 3, 1, 2, 5])); 
+  console.log(cariModus([1, 2, 3, 3, 4, 5]));
+  console.log(cariModus([7, 7, 7, 7, 7])); 
